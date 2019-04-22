7fa634415ed42d6444a9e198499fe3e743bdd481
Fulva1230 <b10503137@gapps.ntust.edu.tw>
2019-04-22 13:31:41 +0800

    delegate job to other activity

--- app/src/main/java/com/noxdawn/remote/Controller.java
+++ app/src/main/java/com/noxdawn/remote/Controller.java
@@ -1,7 +1,17 @@
 package com.noxdawn.remote;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+
+import static android.widget.Toast.LENGTH_SHORT;
+import static com.noxdawn.remote.btconnect.BtItemAdapter.*;
 
 public class Controller extends AppCompatActivity {
     
@@ -9,5 +19,21 @@ public class Controller extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controller);
+        if (getIntent() != null && getIntent().hasExtra(BLUETOOTH_ADDRESS) && getIntent().hasExtra(BLUETOOTH_NAME)) {
+            TextView bluetoothInform = findViewById(R.id.controller_text);
+            bluetoothInform.setText(String.format("connect to : %s", getIntent().getStringExtra(BLUETOOTH_NAME)));
+            BluetoothDevice device = BluetoothAdapter.getDefaultAdapter().getRemoteDevice(getIntent().getStringExtra(BLUETOOTH_ADDRESS));
+            try {
+                BluetoothSocket socket = device.createInsecureRfcommSocketToServiceRecord(SPP_UUID);
+                socket.connect();
+            } catch (IOException e) {
+                Toast toastMessage = Toast.makeText(this, "connect attempt failed", LENGTH_SHORT);
+                toastMessage.show();
+                e.printStackTrace();
+                finish();
+            }
+        } else {
+            finish();
+        }
     }
 }
--- app/src/main/java/com/noxdawn/remote/btconnect/BtItemAdapter.java
+++ app/src/main/java/com/noxdawn/remote/btconnect/BtItemAdapter.java
@@ -3,29 +3,26 @@ package com.noxdawn.remote.btconnect;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 import com.noxdawn.remote.Controller;
 import com.noxdawn.remote.R;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-
-import static android.widget.Toast.LENGTH_SHORT;
 
 public class BtItemAdapter extends BaseAdapter {
     private final BluetoothAdapter bluetoothAdapter;
     private final List<BluetoothDevice> pairedDevices;
     private final Activity parentActivity;
-    private static final UUID SPP_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    public static final UUID SPP_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    public static final String BLUETOOTH_ADDRESS = "bluetooth_address";
+    public static final String BLUETOOTH_NAME = "bluetooth_name";
     
     public BtItemAdapter(BluetoothAdapter bluetoothAdapter, Activity parentActivity) {
         this.bluetoothAdapter = bluetoothAdapter;
@@ -56,20 +53,11 @@ public class BtItemAdapter extends BaseAdapter {
             ((TextView) convertView.findViewById(R.id.textView))
                     .setText(pairedDevices.get(position).getName());
             convertView.setOnClickListener((v) -> {
-                BluetoothDevice device = bluetoothAdapter.getRemoteDevice(pairedDevices.get(position).getAddress());
-                BluetoothSocket socket = null;
-                try {
-                    // Thread acceptThread = new AcceptThread(bluetoothAdapter, SPP_UUID, pairedDevices.get(position).getName());
-                    // acceptThread.start();
-                    socket = device.createInsecureRfcommSocketToServiceRecord(SPP_UUID);
-                    socket.connect();
-                    Intent intent = new Intent(parentActivity, Controller.class);
-                    parentActivity.startActivity(intent);
-                } catch (IOException e) {
-                    Toast toastMessage = Toast.makeText(parentActivity, "connect attempt failed", LENGTH_SHORT);
-                    toastMessage.show();
-                    e.printStackTrace();
-                }
+                Intent intent = new Intent(parentActivity, Controller.class);
+                intent.putExtra(BLUETOOTH_NAME, pairedDevices.get(position).getName());
+                intent.putExtra(BLUETOOTH_ADDRESS, pairedDevices.get(position).getAddress());
+                parentActivity.startActivity(intent);
+                
             });
         }
         return convertView;
